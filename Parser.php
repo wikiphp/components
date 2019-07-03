6c8319355431d27e736b110bacd7c19a5ae2a718
Ben Harris <ben@tilde.team>
2019-07-03 00:23:33 -0400

    init

---
+++ Parser.php
@@ -0,0 +1,18 @@
+<?php
+namespace Wiki;
+
+use Mni\FrontYAML;
+
+class Parser implements FrontYAML\Markdown\MarkdownParser {
+    public function __construct() {
+        $this->mdparser = new WikiParsedown();
+    }
+
+    public function parse($markdown) {
+        return $this->mdparser->text($markdown);
+    }
+
+    public static function factory() {
+        return new FrontYAML\Parser(null, new MDParser());
+    }
+}
